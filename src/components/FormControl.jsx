ac06628af4f00157caace8abd74e2143adacb393
zooyaam <yjoo2498@naver.com>
2024-01-23 10:23:09 +0900

    .

---
+++ src/components/FormControl.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function FormControl() {
+  return (
+    <div className="formControl">
+      <label>
+        사용자 이름 <input type="text" name="username" />
+      </label>
+    </div>
+  );
+}
--- src/lecture/19-reusable-component.jsx
+++ src/lecture/19-reusable-component.jsx
@@ -1,20 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 
-// [학습 주제]
-// - 재사용을 위한 컴포넌트 설계
-
-// .formControl>label+input
-function FormControl() {
-  return (
-    <div className="formControl">
-      <label>
-        사용자 이름 <input type="text" name="username" />
-      </label>
-    </div>
-  );
-}
-
 function App() {
   return (
     <div id="app">
