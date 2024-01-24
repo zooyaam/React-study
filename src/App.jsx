e2e3e18f0c35b89dc01646784c6087039f563c70
zooyaam <yjoo2498@naver.com>
2024-01-24 09:16:45 +0900

    .

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,22 @@ import Widget from './components/Widget/Widget';
 
 function App() {
   return (
-    <div className="app">
+    <div
+      className="app"
+      style={{
+        display: 'grid',
+        gridTemplateColumns: 'repeat(2, 1fr)',
+      }}
+    >
       <Widget
         temperature={-0.9}
         summary="어제보다 1.3° 낮아요"
         location="서울"
       />
       <Widget
-        temperature={-0.7}
+        temperature={-0.2}
         summary="어제보다 0.1° 높아요"
-        location="도쿄"
+        location="부산"
       />
     </div>
   );
